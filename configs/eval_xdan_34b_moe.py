34721dab1eb3121041b3ee77fe1cf6e35e6471a6
xiechengmude <xdan_dev@163.com>
2024-03-23 13:52:57 +0800

    from mmengine.config import read_base
    with read_base():
        from .datasets.gsm8k.gsm8k_gen import  gsm8k_datasets
        from .datasets.math.math_gen import math_datasets
        from .datasets.ceval.ceval_gen import ceval_datasets
        from .datasets.mmlu.mmlu_gen import mmlu_datasets
        from .models.xdan.xdan_hf_4x34b_0318 import moe4x34b_0318_hf
        from .models.xdan.xdan_vllm_4x34b_0318 import moe4x34b_0318_vllm
    
    #datasets = [*gsm8k_datasets, *math_datasets, *ceval_datasets, *mmlu_datasets]
    datasets = [*gsm8k_datasets]
    
    models = [moe4x34b_0318_vllm]

---
+++ configs/eval_xdan_34b_moe.py
@@ -0,0 +1,15 @@
+from mmengine.config import read_base
+
+with read_base():
+    from .datasets.gsm8k.gsm8k_gen import  gsm8k_datasets
+    from .datasets.math.math_gen import math_datasets
+    from .datasets.ceval.ceval_gen import ceval_datasets
+    from .datasets.mmlu.mmlu_gen import mmlu_datasets
+    from .models.xdan.xdan_hf_4x34b_0318 import moe4x34b_0318_hf
+    from .models.xdan.xdan_vllm_4x34b_0318 import moe4x34b_0318_vllm
+
+#datasets = [*gsm8k_datasets, *math_datasets, *ceval_datasets, *mmlu_datasets]
+datasets = [*gsm8k_datasets]
+
+
+models = [moe4x34b_0318_vllm]
