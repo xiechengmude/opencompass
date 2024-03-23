62ad45b44b6d0b14cd4e10c9439100dfc61e9482
xiechengmude <xdan_dev@163.com>
2024-03-23 14:11:03 +0800

    add xDAN-L1-MixChat-v2.1-Agent-0322-e2

--- configs/models/xdan/xdam_vllm_l1_agent_0322.py
+++ configs/models/xdan/xdam_vllm_l1_agent_0322.py
@@ -1,21 +1,28 @@
 from opencompass.models import HuggingFaceCausalLM
+from opencompass.models import VLLM
+
+
+
+_meta_template = dict(
+    begin="<|im_start|>",
+    round=[
+        dict(role="HUMAN", begin="<|im_start|>", end="<|im_end|>"),
+        dict(role="BOT", begin="<|im_start|>", end="<|im_end|>", generate=True),
+    ],
+    eos_token_id=32001
+)
 
 xdan_l1_agent_0322 = [
-    # xDAN-L1-Agent-0322-e2
     dict(
-        type=HuggingFaceCausalLM,
-        abbr='xDAN-L1-Agent-0322-e2',
-        path="xDAN2099/xDAN-L1-MixChat-v2.1-Agent-0322-e2",
-        tokenizer_path='xDAN2099/xDAN-L1-MixChat-v2.1-Agent-0322-e2',
-        tokenizer_kwargs=dict(
-            padding_side='left',
-            truncation_side='left',
-            trust_remote_code=True,
-        ),
-        max_out_len=2048,
+        type=VLLM,
+        abbr='xDAN-L1-MixChat-v2.1-Agent-0322-e2',
+        path='xDAN2099/xDAN-L1-MixChat-v2.1-Agent-0322-e2',
+        meta_template=_meta_template,
+        max_out_len=100,
         max_seq_len=4096,
-        batch_size=4,
-        model_kwargs=dict(trust_remote_code=True, device_map='auto'),
+        batch_size=2,
+        generation_kwargs=dict(temperature=0.7),
+        end_str='<|im_end|>',
         run_cfg=dict(num_gpus=2, num_procs=1),
-    ),
+    )
 ]
