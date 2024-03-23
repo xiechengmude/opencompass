b1258eefd84c10e1afba6dbc335311569c0d99b7
xiechengmude <xdan_dev@163.com>
2024-03-23 13:58:39 +0800

    multi eval for moe

--- configs/eval_xdan_34b_moe.py
+++ configs/eval_xdan_34b_moe.py
@@ -1,10 +1,17 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .datasets.gsm8k.gsm8k_gen import  gsm8k_datasets
-    from .datasets.math.math_gen import math_datasets
-    from .datasets.ceval.ceval_gen import ceval_datasets
-    from .datasets.mmlu.mmlu_gen import mmlu_datasets
+    from .datasets.mmlu.mmlu_gen_a484b3 import mmlu_datasets
+    from .datasets.agieval.agieval_gen_64afd3 import agieval_datasets
+    from .datasets.bbh.bbh_gen_5b92b0 import bbh_datasets
+    from .datasets.gsm8k.gsm8k_gen_1d7fe4 import gsm8k_datasets
+    from .datasets.math.math_evaluatorv2_gen_265cce import math_datasets
+    from .datasets.humaneval.humaneval_gen_8e312c import humaneval_datasets
+    # from .datasets.mbpp.sanitized_mbpp_gen_1e1056 import sanitized_mbpp_datasets
+    # from .datasets.subjective.alpaca_eval.alpacav1_judgeby_gpt4 import subjective_datasets as alpacav1
+    # from .datasets.subjective.alpaca_eval.alpacav2_judgeby_gpt4 import subjective_datasets as alpacav2
+    # from .datasets.subjective.alignbench.alignbench_judgeby_critiquellm import subjective_datasets
+
     from .models.xdan.xdan_hf_4x34b_0318 import moe4x34b_0318_hf
     from .models.xdan.xdan_vllm_4x34b_0318 import moe4x34b_0318_vllm
 
